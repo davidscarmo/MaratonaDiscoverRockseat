e34a8932d304988b3a3a12ef57857f86b1441293
David Carmo <davidsc2812@gmail.com>
2021-02-01 21:46:33 -0300

    Adição, Validação e Exclusão
    Foram adicionados métodos para a criação de uma transação, exclusão e a validação das transações.

--- script.js
+++ script.js
@@ -16,33 +16,34 @@ const Modal = {
     }
   }; 
 
-  const transactions = [
-      {
-       id: 1, 
-       description: 'Luz', 
-       amount: -50000,
-       date: '23/01/2021',   
-      },
-      {
-       id: 2, 
-       description: 'Website', 
-       amount: 500000,
-       date: '23/01/2021',   
-      },
-      {
-       id: 3, 
-       description: 'Internet', 
-       amount: -20000,
-       date: '23/01/2021',   
-      }
-  ]; 
-
   const Transaction = 
   {
-      all: transactions, 
+      all: [
+        {
+         description: 'Luz', 
+         amount: -50000,
+         date: '23/01/2021',   
+        },
+        {
+         description: 'Website', 
+         amount: 500000,
+         date: '23/01/2021',   
+        },
+        {
+         description: 'Internet', 
+         amount: -20000,
+         date: '23/01/2021',   
+        }
+    ], 
       add(transaction)
       {
         Transaction.all.push(transaction);
+        App.reload();
+      },
+      remove(index)
+      {
+        Transaction.all.splice(index, 1); 
+        
         App.reload();
       },
       incomes()
@@ -71,7 +72,7 @@ const Modal = {
       {
         return Transaction.expenses() + Transaction.incomes(); ; 
       }
-  }
+  };
 
   const DOM = 
   {
@@ -106,10 +107,20 @@ const Modal = {
       {
        DOM.transactionContainer.innerHTML = "";   
       }
-  }
+  };
 
   const Utils = 
   {
+      formatAmount(value)
+      {
+        value = Number(value) * 100; 
+        return value;
+      },
+      formatDate(date)
+      {
+        const splittedDate = date.split("-");
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`; 
+      },
       formatCurrency(value)
       {
           const signal = Number(value) <0 ? "-" : ""; 
@@ -123,8 +134,75 @@ const Modal = {
 
           return signal + value; 
       }
-  }
+  };
+  const Form = 
+  {
+    description: document.querySelector("input#description"),
+    amount: document.querySelector("input#amount"),
+    date: document.querySelector("input#date"),
 
+    getValues()
+    {
+      
+      return {
+        description: Form.description.value,
+        amount: Form.amount.value, 
+        date: Form.date.value
+      }
+    },
+    validateField()
+    {
+      const {description, amount, date} = Form.getValues();
+
+     
+      if(description.trim() === "" || amount.trim() === "" || date.trim() === "")
+      {
+        throw new Error("Por favor, preencha todos os campos!"); 
+      }
+    },
+    formatValues()
+    {
+      let { description, amount, date } = Form.getValues();
+      amount = Utils.formatAmount(amount); 
+      
+      date = Utils.formatDate(date); 
+
+      return {
+        description, 
+        amount, 
+        date
+      }
+    },
+    saveTransaction(transaction)
+    {
+      Transaction.add(transaction); 
+    },
+    clearFields()
+    {
+      console.log(Form.description.value);
+      Form.description.value = "", 
+      Form.amount.value = "", 
+      Form.date.value = ""
+    },
+    submit(event)
+    {
+      event.preventDefault();
+
+      try
+      {
+        Form.validateField(); 
+        const transaction = Form.formatValues();
+        Form.saveTransaction(transaction); 
+        Form.clearFields();
+        Modal.close(); 
+      }
+      catch(error)
+      {
+        alert(error.message); 
+      }
+      Form.formatValues();
+    }
+  }; 
   const App = 
   {
       init()
@@ -139,16 +217,8 @@ const Modal = {
         DOM.clearTransaction();
         App.init();
       }
-  }
- 
+  };
   App.init();
-  Transaction.add(
-            {
-                id: 39, 
-                description: 'Alo', 
-                amount: 200, 
-                date: '23/01/2021'
-            }
-        ); 
-       
-console.log(transactions);
+
+
+
